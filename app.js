d09638f27276069e55ee496878fc1664ede4547a
Julian Vallejos <jevallejos99@gmail.com>
2022-02-07 01:18:13 -0300

    started gameManager development

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
 export class Piece {
   constructor(type,id){
   this.type=type;
+  this.color = "white";
   if (id<16)
     this.color = "black";
-  else
-    this.color = "white";
   this.moveCount=0;
-  this.id=0;
+  this.id=id;
   }
 }
 
 export class Board {
   constructor(){
     let squareArray = new Array(64)
-    this.squares = squareArray.fill(new Piece("0")) 
+    this.squares = squareArray.fill(new Piece("empty",-1)) 
     this.blackPieces = 16;
     this.whitePieces = 16;
     this.turnCount = 0;
     }
-    initializeBoard(){
-      this.squares[0]= this.squares[7]= new Piece("r","black")
-      this.squares[56]= this.squares[63]= new Piece("r","white")
-      this.squares[1]= this.squares[6]= new Piece("kn","black")
-      this.squares[57]= this.squares[62]= new Piece("kn","white")
-      this.squares[2]= this.squares[5]= new Piece("b","black")
-      this.squares[58]= this.squares[61]= new Piece("b","white")
-      this.squares[3]= new Piece("q","black") 
-      this.squares[59]= new Piece("q","white")
-      this.squares[4]= new Piece("k","black")
-      this.squares[60]= new Piece("k","white")
-      for (let i = 0; i < 8; i++) {
-        this.squares[8+i] = new Piece("p","black")
-        this.squares[48+i] = new Piece("p","white")
+  initializeBoard(){
+    for (let i = 0; i<65; i++) {
+      if (i>15 && i<48)
+        continue
+      switch (i) {
+        case 0:
+        case 7:
+        case 56:
+        case 63:
+          addPiece(this,i,"r")
+          break;
+        case 1:
+        case 6:
+        case 57:
+        case 62:
+          addPiece(this,i,"kn")
+          break;
+        case 2:
+        case 5:
+        case 58:
+        case 61:
+          addPiece(this,i,"b")
+          break;
+        case 3:
+        case 59:
+          addPiece(this,i,"q")
+          break;
+        case 4:
+        case 60:
+          addPiece(this,i,"k")
+          break;
+        default:
+          addPiece(this,i,"p")
+          break;
       }
     }
   }
+}
+function addPiece(board,id,type) {
+  board.squares[id]=new Piece(type,id)
+}
--- main.js
+++ main.js
@@ -1,95 +1,114 @@
 import {Piece,Board} from "./app.js";
+
 let board = new Board()
 board.initializeBoard();
 
-// function addPiece(board,id,type) {
-//   board.squares[i]=new Piece(type,id)
-// }
-
-// for (let i = 0; i < 17; i++) {
-//   if (i>15 && i<48)
-//     continue
-//   switch (i) {
-//     case 0:
-//     case 7:
-//       addPiece(this,i,"r")
-//       break;
-//     case 1:
-//     case 6:
-//       addPiece("this,i,kn")
-//       break;
-//     case 2:
-//     case 5:
-//       addPiece(this,i,"b")
-//       break;
-//     case 3:
-//       addPiece(this,i,"q")
-//       break;
-//     case 4:
-//       addPiece(this,i,"k")
-//       break;
-//     default:
-//       addPiece(this,i,"p")
-//       break;
-//   }
-// }
+let gameManager = {};
+let gameManagerProxy = new Proxy (gameManager,{
+  set: function (target,key,value) {
+    if (key == "selectedSquare" && !(target[key] === value)) {
+      let previousSquareObj = $("#"+target[key]);
+      previousSquareObj.removeClass("border-warning");
+      previousSquareObj.addClass("border-danger");
+      previousSquareObj.addClass("border-dark");
+      previousSquareObj.on({
+        mouseenter: function () {
+          previousSquareObj.removeClass("border-dark");
+        },
+        mouseleave: function () {
+          previousSquareObj.addClass("border-dark");
+        }
+      });
+      let currentSquareObj=$("#"+value);
+      currentSquareObj.removeClass("border-danger");
+      currentSquareObj.addClass("border-warning");
+      currentSquareObj.off("mouseenter mouseleave");
+      checkMoves(currentSquareObj.attr("id"),currentSquareObj.children().attr("id"))
+    }
+    target[key]=value;
+    return true
+  }
+})
 
 $(()=>{
-$("#renderButton").click(()=>{console.log("renderburon")})
-console.log(board);
-$("#startButton").fadeIn();
-$("#startButton").click(()=>{
-  console.log("started")
-  $("#startButton").fadeOut(()=>{
-    $(".body").append(`<div class="border border-dark border-2 mx-auto m-2" style="display: none;" id="board"></div>`);
-    createBoard();
-    $("#board").fadeIn(renderPieces(board));
-    $("#buttonContainer").remove();
+  $("#renderButton").click(()=>{console.log(gameManagerProxy.selectedSquare)})
+  console.log(board);
+  $("#startButton").fadeIn();
+  $("#startButton").click(()=>{
+    console.log("started")
+    $("#startButton").fadeOut(()=>{
+      $(".body").append(`<div class="border border-dark border-2 mx-auto m-2" style="display: none;" id="board"></div>`);
+      createBoard();
+      $("#board").fadeIn(renderPieces(board));
+      $("#buttonContainer").remove();
+    })
   })
-})
-$("#moveButton").click(()=>{
-  let pieza = new Piece("q","black")
-  console.log(pieza);
-})
 })
 
 function renderPieces(board){
-let tableSquares = $(".square")
-board.squares.forEach((element,i) => {
-  if (element.type!="0") {
-    $("#"+i).children().replaceWith(`<img class="piece img-fluid position-absolute top-50 start-50 translate-middle" id="${element.type}" src="img/${element.type}${element.color}.png"></img>`)
-  }
-});
-tableSquares.hover((e)=>{
-  $("#"+e.currentTarget.id).removeClass("border-dark");
-  $("#"+e.currentTarget.id).addClass("border-danger");
-},(e)=>{
-  $("#"+e.currentTarget.id).removeClass("border-danger");
-  $("#"+e.currentTarget.id).addClass("border-dark");
-});
+  let tableSquares = $(".square")
+  board.squares.forEach((element,i) => {
+    if (element.type!="empty") {
+      $("#"+i).children().replaceWith(`<img class="piece img-fluid position-absolute top-50 start-50 translate-middle" id="${element.type}" src="img/${element.type}${element.color}.png"></img>`)
+      $("#"+i).click(()=>{
+        gameManagerProxy.selectedSquare = i;
+      })
+      $("#"+i).on({
+        mouseenter: function () {
+          $("#"+i).removeClass("border-dark");
+        },
+        mouseleave: function () {
+          $("#"+i).addClass("border-dark");
+        }
+      });
+    }
+  });
 }
 
 function createBoard(){
-let table = $("#board");
-let counter = 0;
-for (let index = 1; index < 9; index++) {
-  if(index%2){
-    for (let i = 1; i < 9; i++) {
-      if(i%2)
-      table.append(`<div class="border border-2 border-dark position-relative square white" id="${counter}"><div></div></div>`);
-      else
-      table.append(`<div class="border border-2 border-dark position-relative square black" id="${counter}"><div></div></div>`);
-      counter++;
+  let table = $("#board");
+  let counter = 0;
+  for (let index = 1; index < 9; index++) {
+    if(index%2){
+      for (let i = 1; i < 9; i++) {
+        if(i%2)
+        table.append(`<div class="border border-2 border-danger border-dark position-relative square white" id="${counter}"><div></div></div>`);
+        else
+        table.append(`<div class="border border-2 border-danger border-dark position-relative square black" id="${counter}"><div></div></div>`);
+        counter++;
+      }
     }
-  }
-  else{
-    for (let i = 1; i < 9; i++) {
-      if(i%2)
-      table.append(`<div class="border border-2 border-dark position-relative square black" id="${counter}"><div></div></div>`);
-      else
-      table.append(`<div class="border border-2 border-dark position-relative square white" id="${counter}"><div></div></div>`);
-      counter++;
+    else{
+      for (let i = 1; i < 9; i++) {
+        if(i%2)
+        table.append(`<div class="border border-2 border-danger border-dark position-relative square black" id="${counter}"><div></div></div>`);
+        else
+        table.append(`<div class="border border-2 border-danger border-dark position-relative square white" id="${counter}"><div></div></div>`);
+        counter++;
+      }
     }
   }
 }
+
+function checkMoves(currentPosition,pieceType) {
+  
+  console.log("current position "+currentPosition);
+  console.log("pieceType "+pieceType);
+  let possibleMoves = []
+  let piece = board.squares[currentPosition];
+  switch (piece.type) {
+    case "p":
+      if (piece.color == "black") {
+        possibleMoves.push(parseInt(currentPosition)+8)
+        if (piece.moveCount == 0) {possibleMoves.push(parseInt(currentPosition)+16)}
+        console.log(possibleMoves);
+        possibleMoves.forEach(element => {
+          $("#"+element).addClass("selected");
+          $("#"+element).click(()=>{
+            // tryMove(element,currentPosition)
+          })
+        });
+      }
+      break;
+  }
 }
